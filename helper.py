260228d04844830487b0f580bf801d42d42d497e
aitadioptoure <aitoussha@gmail.com>
2023-07-12 16:21:24 +0000

    first commit

---
+++ helper.py
@@ -0,0 +1,56 @@
+from urlextract import URLExtract
+from wordcloud import WordCloud
+extract = URLExtract()
+
+def fetch_stats(selected_user,df):
+
+    if selected_user != 'Overall':
+      df = df[df['user'] == selected_user]
+
+    #fetch the number of message
+    num_messages = df.shape[0]
+
+    # fetch the total number of words
+    words = []
+    for message in df['message']:
+       words.extend(message.split())
+
+    # fetch number of media message
+    num_media_messages =  df[df['message'] == '<Media Omitted>\n'].shape[0]
+
+    #fetch number of links shared
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+
+    #fetch number pf links shared
+    return num_messages, len(words), num_media_messages, len(links)
+
+def most_busy_users(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+
+    return x, df
+def create_wordcloud(selected_user,df):
+
+    #f = open('stop_hinglish.txt', 'r')
+    #stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+    return df_wc
+
+    #temp = df[df['user'] != 'group_notification']
+    #temp = temp[temp['message'] != '<Media omitted>\n']
+
+    #def remove_stop_words(message):
+       # y = []
+        #for word in message.lower().split():
+        #    if word not in stop_words:
+          #      y.append(word)
+        #return " ".join(y)
